b3cae77b7786bf4facb745e8b8901d87c7ef7e1b
hybridarjun <sachdevaarjun29@gmail.com>
2022-09-25 17:22:51 -0400

    config env

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -3,24 +3,24 @@ package db
 import (
 	"database/sql"
 	"log"
+	"newmigrate/util"
 	"os"
 	"testing"
 
 	_ "github.com/lib/pq"
 )
 
-const (
-	dbDriver = "postgres"
-	dbSource = "postgresql://root:secretpassword@localhost:5432/simple_bank?sslmode=disable"
-)
-
 var testQueries *Queries
 var testDB *sql.DB
 
 func TestMain(m *testing.M) {
-	var err error
+	config, err := util.LoadConfig("../..")
 
-	testDB, err = sql.Open(dbDriver, dbSource)
+	if err != nil {
+		log.Fatal("cannot load config:", err)
+	}
+
+	testDB, err = sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("cannot connect to db:", err)
 	}
